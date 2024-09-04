e0c2888fa0bf7840dcfc2238ca6df5363c5b3552
wannipabam <66309010018@thonburi.ac.th>
2024-09-04 10:48:50 +0700

    my first commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script setup>
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+import MyNev from './components/MyNev.vue'
+import MyCard from './components/MyCard.vue'
+import items from './mune.json'
+</script>
+
+<template>
+<div>
+  <header>
+    <MyNev/>
+
+    
+    
+
+
+    <!--<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>-->
+  </header>
+
+  <main>
+    <TheWelcome />
+    <div className="grid-container">
+      <MyCard v-bind="items[0]"/>
+      <MyCard v-bind="items[1]"/>
+      <MyCard v-bind="items[2]"/>
+      <MyCard v-bind="items[3]"/>
+      <MyCard v-bind="items[4]"/>
+      <MyCard v-bind="items[5]"/>
+      <MyCard v-bind="items[6]"/>
+      <MyCard v-bind="items[7]"/>
+      <MyCard v-bind="items[8]"/>
+      <MyCard v-bind="items[9]"/>
+      <MyCard v-bind="items[10]"/>
+      <MyCard v-bind="items[11]"/>
+    </div>
+
+  </main>
+</div>
+</template>
+
+<style scoped>
+
+*{
+  background-color: darkkhaki;
+}
+
+/*header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}*/
+</style>
---
+++ src/components/MyCard.vue
@@ -0,0 +1,50 @@
+<template lang="">
+    <div>
+
+      <div class="card">
+        <img :src="image" alt="name" style="max-width: 500px; width:100%" >
+            <div class="container">
+                <h4><b>{{ southern }}</b></h4>
+                <p>{{ information }}</p>
+                <p>{{ month }}</p>
+            </div>
+            
+      </div>  
+    </div>
+</template>
+<script>
+export default {
+    props:['southern','information','month','image']
+    
+}
+</script>
+<style scoped>
+
+*{
+  text-align: center;
+  box-sizing: border-box;
+  padding: 2%;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgb(163, 4, 4);
+  transition: 0.3s;
+  width: 80%;
+  height: 100%;
+  font-size: 22px;
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: rgb(245, 209, 120);
+  color: rgb(158, 117, 12);
+}
+
+.card:hover {
+  box-shadow: 0 5px 10px 0 rgba(223, 162, 31, 0.897);
+}
+
+.container {
+  padding: 2px 16px;
+}
+    
+</style>
